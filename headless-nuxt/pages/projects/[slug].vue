3d0caaede81996ee622cd6ce4d720c9246729cdb
QuentinLegeron <quentin.legeron@gmail.com>
2023-03-05 02:32:05 +0100

    start adding style

--- headless-nuxt/pages/projects/[slug].vue
+++ headless-nuxt/pages/projects/[slug].vue
@@ -1,19 +1,11 @@
-<template>
+<!-- <template>
     <div v-if="project">
-        <nuxt-link to="/">
-            Retour
-        </nuxt-link>
-        <br>
         titre : {{project.title}}
-        <br>
         type : {{ project.type }}
-        <br>
         <div>
             description : {{ project.description }}
         </div>
-        <br>
         <img style="width: 100px;" :src='`${project.img.url}`' :alt="`${ project.img.name }`">
-        <br>
         <span>Technologies :</span>
         <ul>
             <li v-for="technology in project.technologies" :key="technology.id">
@@ -21,8 +13,26 @@
                 <img style="width: 50px;" :src='`${technology.image.url}`' :alt="`${ technology.image.name }`">
             </li>
         </ul>
-        <br>
         <nuxt-link :to="project.link" target="_blank">Lien du projet</nuxt-link>
+    </div>
+</template> -->
+
+<template>
+    <div v-if="project" class="project-details">
+        <h1>{{project.title}}</h1>
+        <div class="project-type">{{ project.type }}</div>
+        <div class="project-description">{{ project.description }}</div>
+        <img class="project-img" :src='`${project.img.url}`' :alt="`${ project.img.name }`">
+        <div v-if="project.technologies.lenght > 0">
+            <span>Technologies :</span>
+            <ul class="technology-list">
+                <li v-for="technology in project.technologies" :key="technology.id" class="technology-item">
+                    {{ technology.name }}
+                    <img :src='`${technology.image.url}`' :alt="`${ technology.image.name }`">
+                </li>
+            </ul>
+        </div>
+        <a :href="project.link" class="project-link" target="_blank">Lien du projet</a>
     </div>
 </template>
 
@@ -39,4 +49,70 @@ onMounted(async () => {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+    .project-details {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        padding: 20px;
+        background-color: #f5f5f5;
+        border-radius: 10px;
+        box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+    }
+
+    .project-details h2 {
+        font-size: 30px;
+        margin-bottom: 20px;
+    }
+
+    .project-type {
+        font-size: 20px;
+        margin-bottom: 10px;
+    }
+
+    .project-description {
+        font-size: 16px;
+        margin-bottom: 20px;
+    }
+
+    .project-img {
+        width: 100%;
+        max-width: 550px;
+        margin-bottom: 20px;
+        border-radius: 10px;
+        box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+    }
+
+    .technology-list {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-bottom: 20px;
+    }
+
+    .technology-item {
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+    }
+
+    .technology-item img {
+        width: 30px;
+        margin-left: 5px;
+    }
+
+    .project-link {
+        font-size: 16px;
+        color: #0077cc;
+        text-decoration: none;
+    }
+
+    .project-link:hover {
+        text-decoration: underline;
+    }
+</style>
